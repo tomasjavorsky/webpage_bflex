e7ac341f9fbb09555d2311ebfe3e35bc5f7de546
Tomáš Javorský <7Tomus@users.noreply.github.com>
2019-06-19 20:29:06 +0200

    Layout without components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+
+      {/*Header*/}
+      <div className={"header"}>
+        <h1>Bflex, s.r.o.</h1>
+        <p>Predaj a technické poradenstvo</p>
+      </div>
+
+      {/*Navigation Bar*/}
+      <div className={"navbar"}>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+      </div>
+
+      {/*Content*/}
+      <div className={"row"}>
+
+        <div className={"left-panel"}>
+          <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+
+        <div className={'main'}>
+          <h2>TITLE HEADING</h2>
+          <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco.</p>
+        </div>
+      </div>
+
+      {/*Footer*/}
+      <div class="footer">
+        <p>Footer</p>
+      </div>
     </div>
   );
 }
