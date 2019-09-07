e216f8b9a6a61ca0b32beda24a532226276726fc
Tomáš Javorský <7Tomus@users.noreply.github.com>
2019-09-07 13:42:47 +0200

    strings update

--- src/strings.js
+++ src/strings.js
@@ -11,23 +11,15 @@ const texts = {
   search: "Hľadať",
   companyName: "Bflex, s.r.o.",
   companySlogan: "Predaj a technické poradenstvo",
-  companyContactInfo:
-    <div>
-        <p>Ľ. Fullu 7, 841 05 Bratislava</p>
-        <p>tel. +421 2 20721062</p>
-        <p>fax.: +421 2 20730795</p>
-        <p>mob.: +421 917 701028</p>
-        <p>e-mail: info@bflex.sk</p>
-    </div>,
   create: "Vytvoriť",
   cancel: "Zrušiť",
   delete: "✖",
   productNamePlaceholder: "Názov produktu",
   productDescriptionPlaceholder: "Popis produktu",
-  productTagsPlaceholder: "Tagy (oddelovať s ,)",
+  productTagsPlaceholder: "Tagy",
   chooseCategoryPlaceholder: "Vybrať kategóriu",
-  tableColumnsPlaceholder: "Stĺpce tabulky (oddelovať s ,)",
-  tableRowsPlaceholder: "Riadky tabulky (oddelovať s ,)",
+  tableColumnsPlaceholder: "Stĺpce tabulky (oddelovať s ;)",
+  tableRowsPlaceholder: "Riadky tabulky (oddelovať s ;)",
   login: "Prihlásiť sa",
   namePlaceholder: "Meno",
   passwordPlaceholder: "Heslo",
